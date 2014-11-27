671fc1d5bc37ff813931e79d24ec4765ae365a5b
Elrhino <renlane@hotmail.com>
2014-11-26 23:06:09 -0500

    Corrected inverted functions name

--- Valid_PAN_Format/valid_pan_format.py
+++ Valid_PAN_Format/valid_pan_format.py
@@ -1,34 +1,40 @@
 def main():
     nb_tests = int(input())
+
     for i in range(0, nb_tests):
         pan = PanNumber(input())
-        print(pan.validate())
+
+        if pan.isValid:
+            print("YES")
+        else:
+            print("NO")
 
 
 class PanNumber():
 
     def __init__(self, pan_number):
         self.pan = pan_number
+        self.isValid = False
+        self._validate()
 
-    def validate(self):
-        output = 'NO'
+    def _validate(self):
         if self.pan.isupper():
             if self._validate_digits() and self._validate_alpha():    
-                output = 'YES'
-
-        return output
+                self.isValid = True
 
     def _validate_digits(self):
-        if self.pan[:5].isalpha() and self.pan[-1:].isalpha():
-            return True
+        isValid = False
+        if self.pan[5:9].isnumeric():
+            isValid = True
 
-        return False
+        return isValid
 
     def _validate_alpha(self):
-        if self.pan[5:9].isnumeric():
-            return True
+        isValid = False
+        if self.pan[:5].isalpha() and self.pan[-1:].isalpha():
+            isValid = True
 
-        return False
+        return isValid
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
